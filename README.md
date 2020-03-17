7ff6ebfa1c6c34703c6bba017c3d57661a53a9ce
HarshadRanganathan <rharshad93@gmail.com>
2020-03-17 14:23:16 +0000

    Sample app

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # aws-eb-spring-boot-fluentd-kinesis-example-app
-Sample spring boot application for AWS elasticbeanstalk with fluentd and kinesis configurations
+Sample spring boot application for AWS elasticbeanstalk with fluentd and kinesis configurations.
+
+This application exposes two endpoints:
+
+|Path|Description|
+|---|---|
+|/helloworld|Returns hello world message|
+|/err|Throws ArithmeticException and returns error response|
+
+To build the jar artifact:
+
+```
+mvn package spring-boot:repackage
+```
+
+To run it in your local:
+
+```
+java -jar aws-eb-spring-boot-fluentd-kinesis-example-app-1.0-SNAPSHOT.jar
+```
---
+++ src/main/java/com/eb/RestController.java
@@ -0,0 +1,33 @@
+package com.eb;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collections;
+import java.util.Map;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+    private static final Logger logger =  LoggerFactory.getLogger(RestController.class);
+
+    @GetMapping(value = "/helloworld", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> helloWorld() {
+        return new ResponseEntity<>("{\"message\": \"Hello World\"}", HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/err", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> error() {
+        try {
+            final int divideByZero = 10 / 0;
+        } catch (ArithmeticException e) {
+            logger.error("Runtime error occurred", e);
+        }
+        return new ResponseEntity<>("{\"error\": \"Runtime Error\"}", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
